ab1a5a00b720088cdf615e3bcd8298b8f771634d
Phani Chandra Sourabh Reddy Reddy <sourabhreddy369@gmail.com>
2020-03-20 03:33:27 -0400

    Added week 7 python assignment 1

---
+++ Week7_Assignment1_Workbook_1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import pandas as pd 
+Location = "C:/Users/soura/OneDrive/Desktop/Data Visualization/datasets/all_040_in_12.P1.csv" 
+df = pd.read_csv(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Week7_Assignment1_Workbook_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import pandas as pd 
+Location = "C:/Users/soura/OneDrive/Desktop/Data Visualization/datasets/BPS01.xlsx" 
+df = pd.read_excel(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
